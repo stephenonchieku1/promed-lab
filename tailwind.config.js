5bc190a1ee82319326ea19392147528a0af054d7
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-06-06 17:19:50 +0300

    "Updated package-lock.json, package.json, App.js, and tailwind.config.js files"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+
+    
+  ],
 }
 
